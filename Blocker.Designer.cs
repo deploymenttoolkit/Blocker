d2adc7b496f73705aaf5ee96de8fea8c50f652f3
Gérald Döserich <github@doeseri.ch>
2021-12-27 17:30:16 +0100

    Retarget to .NET6.0

--- Blocker.Designer.cs
+++ Blocker.Designer.cs
@@ -39,10 +39,11 @@
             // 
             this.LabelInstallation.BackColor = System.Drawing.Color.Transparent;
             this.LabelInstallation.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LabelInstallation.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelInstallation.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.LabelInstallation.Location = new System.Drawing.Point(0, 0);
+            this.LabelInstallation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LabelInstallation.Name = "LabelInstallation";
-            this.LabelInstallation.Size = new System.Drawing.Size(800, 461);
+            this.LabelInstallation.Size = new System.Drawing.Size(933, 532);
             this.LabelInstallation.TabIndex = 0;
             this.LabelInstallation.Text = "Installation running\n";
             this.LabelInstallation.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -54,8 +55,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.PictureLogo.BackColor = System.Drawing.Color.Transparent;
             this.PictureLogo.Location = new System.Drawing.Point(0, 0);
+            this.PictureLogo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.PictureLogo.Name = "PictureLogo";
-            this.PictureLogo.Size = new System.Drawing.Size(800, 100);
+            this.PictureLogo.Size = new System.Drawing.Size(933, 115);
             this.PictureLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.PictureLogo.TabIndex = 1;
             this.PictureLogo.TabStop = false;
@@ -68,21 +70,24 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.PictureBackground.BackColor = System.Drawing.Color.Transparent;
             this.PictureBackground.Location = new System.Drawing.Point(0, 0);
+            this.PictureBackground.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.PictureBackground.Name = "PictureBackground";
-            this.PictureBackground.Size = new System.Drawing.Size(800, 461);
+            this.PictureBackground.Size = new System.Drawing.Size(933, 532);
             this.PictureBackground.TabIndex = 2;
             this.PictureBackground.TabStop = false;
             this.PictureBackground.UseWaitCursor = true;
             // 
             // Blocker
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 461);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(933, 532);
             this.ControlBox = false;
             this.Controls.Add(this.PictureLogo);
             this.Controls.Add(this.LabelInstallation);
             this.Controls.Add(this.PictureBackground);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Blocker";
